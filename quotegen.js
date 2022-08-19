73da7a203ad9da8ca4952904013f4a7b915f0ba2
Nicolas DiTullio Lemaire <80335821+1scfz@users.noreply.github.com>
2022-08-19 14:44:35 -0400

    Change link colours

--- quotegen.js
+++ quotegen.js
@@ -1,8 +1,8 @@
 var quotes = [
     "Buy your yearbook NOW",
     "Buy your yearbook. Like, now.",
-    "If you're a senior, don't forget to check out our <a href=\"todo.html\">Senior To-Do List</a>",
-    "If you have any pictures to upload to the yearbook, go to <a href=\"hjeshare.com\">hjeshare.com</a>"
+    "If you're a senior, don't forget to check out our <a style=\"color: lightyellow;\" href=\"todo.html\">Senior To-Do List</a>",
+    "If you have any pictures to upload to the yearbook, go to <a style=\"color: lightyellow;\" href=\"hjeshare.com\">hjeshare.com</a>"
 ]
 
 function newQuote() {
