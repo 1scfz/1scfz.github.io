4f7a953f703a4efea05b516e50f2944ebcaba9d7
Nicolas DiTullio Lemaire <80335821+1scfz@users.noreply.github.com>
2022-08-19 14:33:35 -0400

    Fixed quote

--- quotegen.js
+++ quotegen.js
@@ -2,7 +2,7 @@ var quotes = [
     "Buy your yearbook NOW",
     "Buy your yearbook. Like, now.",
     "If you're a senior, don't forget to check out our Senior To-Do List",
-    "If you have any pictures to upload to the yearbook, go to <LINK HERE>"
+    "If you have any pictures to upload to the yearbook, go to <a href=\"hjeshare.com\">hjeshare.com</a>"
 ]
 
 function newQuote() {
